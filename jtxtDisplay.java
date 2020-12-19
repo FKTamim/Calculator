7b0667af18c52a205f7ca4f3348123d0d7251a6d
FKTamim <76146392+FKTamim@users.noreply.github.com>
2020-12-19 18:28:27 +0600

    Add files via upload

---
+++ jtxtDisplay.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CalculatorRoundbutton;
+
+/**
+ *
+ * @author Tamim
+ */
+class jtxtDisplay {
+
+    static void setText(String answer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void getText(String EnterNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
